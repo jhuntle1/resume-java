d065898bff4f9586ca9ea7ec413052e2a1c9f0a6
InnovateBham_8 <innovatebham_8@UABiLAB-8s-iMac.local>
2017-06-15 17:15:58 -0500

    better formatting

---
+++ resume.js
@@ -0,0 +1,37 @@
+console.log('Name: Justin Timothy Huntley'.toUpperCase()) 
+console.log('Birmingham, AL');
+console.log('Java Developer');
+console.log('I like turtles');
+console.log('My Interests:');
+console.log('* Rap Music');
+console.log('* Sleeping');
+console.log('* Videogames');
+console.log('* Coding');
+console.log('My Previous Experience:');
+
+
+function displayPosition(companyName, jobTitle, description) {
+  console.log('Worked at ' + companyName + ' as a ' + jobTitle + ' ' + description);
+} 
+
+displayPosition('Sams Club' , 'Cart Attendant', 'where I would avoid work as much as possible');
+displayPosition('Hyundai Glovis' , 'Shuttle Driver', 'where I would avoid work as much as possible');
+
+function displaySkill(skill, isCool){
+    console.log(skill);
+    console.log(isCool);
+
+    if (isCool ==true) {
+        console.log('Check It' = isCool);
+    } else {
+        console.log(skill)
+    }
+}
+
+('My Skills:');
+displaySkill('Sleeping');
+console.log('Hearing without listening');
+console.log('An amazing freestyler');
+
+
+ 
